b876e0fc430e9016dbe637bbe3654548ece6c0de
JSH-data <super60447@gmail.com>
2021-04-08 18:06:30 +0900

    Django Tutorial Finish

--- Todo_app/urls.py
+++ Todo_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', views.TodoCV.as_view(), name='create'),
     path('list/', views.TodoLV.as_view(), name='list'),
     path('<int:pk>/delete/', views.TodoDelV.as_view(), name='delete'), # <int:pk> path converter는 숫자를 인자로 받아서 view에 넘겨줍니다. 
+    path('mixin/', views.TodoMOMCV.as_view(), name='mixin'),
+    path('<int:pk>/delete2/', views.TodoDelv2.as_view(), name='delete2')
 ]
--- Todo_app/views.py
+++ Todo_app/views.py
@@ -1,7 +1,21 @@
 from django.views.generic import TemplateView, CreateView, ListView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 from Todo_app.models import Todo
 from django.urls import reverse_lazy
 
+class TodoMOMCV(MultipleObjectMixin, CreateView):
+    model = Todo
+    fields = '__all__'
+    template_name = 'Todo_app/todo_mixin.html'
+    success_url = reverse_lazy('Todo_app:mixin') 
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset() 
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().post(request, *args, **kwargs)
 
 class TodoVueOnlyTV(TemplateView) :
     template_name = 'Todo_app/todo_vue_only.html'
@@ -13,10 +27,18 @@ class TodoCV(CreateView) :
     success_url = reverse_lazy('Todo_app:list')
 
 class TodoLV(ListView) :
-    model = Todo
+    model = Todo # 리스트 뷰를 사용함으로서 Todo라는 데이터베이스의 자료를 자동으로 템플릿에게 넘겨주게 됩니다. 
     template_name = 'Todo_app/todo_list.html'
 
 class TodoDelV(DeleteView) :
     model = Todo
     template_name = 'Todo_app/todo_confirm_delete.html'
     success_url = reverse_lazy('Todo_app:list')
+
+class TodoDelv2(DeleteView) :
+    model = Todo
+    # template_name = 'Todo_app/todo_confirm_delete.html'
+    success_url = reverse_lazy('Todo_app:mixin')
+
+    def get(self, request, *args, **kwargs) :
+        return self.delete(request, *args, **kwargs)
